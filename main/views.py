55534a13fc5325271daa5fba768eb19863ad7705
joan-w <wamuyujoan4@gmail.com>
2020-06-07 17:36:07 +0300

    Creating a home view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+def home(request):
+    allProjects = Project.objects.all() # selects everything from the Project table
+    
+    context = {
+        'projects' : allProjects,
+    }
+
+    return render(request, 'main/index.html', context)
