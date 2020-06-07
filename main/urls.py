26fa9c36cd4035841d1d6156afb828aa5204f43d
joan-w <wamuyujoan4@gmail.com>
2020-06-07 23:39:36 +0300

    Adding an app_name

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     url(r'', views.home, name='home'),
-    url(r'^details/<int:id>/', views.detail, name="detail"),
+    url(r'^details/<int:id>/', views.detail, name="detail")
 ]
