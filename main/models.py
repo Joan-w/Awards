ee95458d205165694a6142fc550ee5b92b339437
joan-w <wamuyujoan4@gmail.com>
2020-06-07 15:13:53 +0300

    Adding the fields of a project table

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    # fields for the project table
+    name = models.CharField(max_length=300)
+    languages = models.CharField(max_length=300)
+    description = models.TextField(max_length=5000)
+    projectLink = models.CharField(max_length=300)
+    averageRating = models.FloatField()
+
+    def __str__(self):
+        return self.name
