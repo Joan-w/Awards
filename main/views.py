d092d2c4f0671abf09ae8cace7bb22e661184d33
joan-w <wamuyujoan4@gmail.com>
2020-06-08 11:53:23 +0300

    Updating the add_projects view

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import *
 
 # Create your views here.
 def home(request):
@@ -11,6 +12,7 @@ def home(request):
     }
     return render(request, 'main/index.html', context)
 
+# detail page
 def detail(request, id):
     project = Project.objects.get(id=id) # Selects everything from the Project table where id=id
 
@@ -18,3 +20,17 @@ def detail(request, id):
         "project" : project,
     }
     return render(request, 'main/details.html', context)
+
+# add projects to the database
+def add_projects(request):
+    if request.method == 'POST':
+        form = ProjectForm(request.POST or None)
+
+        # check if form is valid
+        if form.is_valid():
+            data = form.save(commit=False)
+            data.save()
+            return redirect('main:home')
+    else:
+        form = ProjectForm()
+    return render(request, 'main/addprojects.html', {"form":form})
