b6dce9ec738a2dff1c377883fbbad9de65a1d40a
joan-w <wamuyujoan4@gmail.com>
2020-06-08 11:54:57 +0300

    Creating a ProjectForm

---
+++ main/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import *
+
+# Project add form
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = (
+            'name',
+            'image',
+            'reviewDate',
+            'languages',
+            'description',
+            'projectLink')
