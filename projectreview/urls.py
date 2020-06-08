d9172f897232eb276c7b4908a83f418f324e4b8c
joan-w <wamuyujoan4@gmail.com>
2020-06-08 13:15:19 +0300

    Including the accounts' urls

--- projectreview/urls.py
+++ projectreview/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('main.urls')),
+    url(r'^accounts/', include('accounts.urls'))
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
