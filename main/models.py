eaab54269b8cc98ed9a3b183e36968be1726d182
joan-w <wamuyujoan4@gmail.com>
2020-06-08 11:52:00 +0300

    Updating the image field

--- main/models.py
+++ main/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Project(models.Model):
     # fields for the project table
     name = models.CharField(max_length=300)
-    image = models.ImageField(upload_to='images/', null=True)
-    reviewDate = models.DateField()
+    image = models.ImageField(blank=True, null=True)
+    reviewDate = models.DateTimeField(default=timezone.now())
     languages = models.CharField(max_length=300)
     description = models.TextField(max_length=5000)
     projectLink = models.CharField(max_length=300)
-    averageRating = models.FloatField()
+    averageRating = models.FloatField(default=0)
 
     def __str__(self):
         return self.name
