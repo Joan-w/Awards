76d1901063794128b2214b059a4b564b7b772cd4
joan-w <wamuyujoan4@gmail.com>
2020-06-09 04:46:46 +0300

    modify Project model

--- main/models.py
+++ main/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Project(models.Model):
     # fields for the project table
     name = models.CharField(max_length=300)
-    image = models.ImageField()
+    image = models.ImageField(blank=True, null=True)
     reviewDate = models.DateTimeField(default=timezone.now())
     languages = models.CharField(max_length=300)
     description = models.TextField(max_length=5000)
@@ -18,7 +18,7 @@ class Project(models.Model):
 
 class Review(models.Model):
     # fields for the review table
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    # project = models.ForeignKey(Project, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment = models.TextField(max_length=1000)
     rating = models.FloatField(default=0)
